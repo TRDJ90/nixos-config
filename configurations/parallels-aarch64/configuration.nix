5621758c6596254611e16fac594e25e3f7b6d88c
Thubie de Jong <thubie@Thubies-MacBook-Pro.local>
2022-10-12 22:00:47 +0200

    Corrected pkgs path

--- configurations/parallels-aarch64/configuration.nix
+++ configurations/parallels-aarch64/configuration.nix
@@ -13,7 +13,7 @@
   disabledModules = [ "virtualisation/parallels-guest.nix" ];
   hardware.parallels = {
     enable = true;
-    package = (config.boot.kernelPackages.callPackage ../pkgs/parallels-tools/default.nix { });
+    package = (config.boot.kernelPackages.callPackage ../../pkgs/parallels-tools/default.nix { });
   };
 
   # Interface is this on my M1
