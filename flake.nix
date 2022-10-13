26c6f3cd46c3f3d2907526619668f4c1688ec1e7
Thubie de Jong <thubie@Thubies-MacBook-Pro.local>
2022-10-13 13:45:28 +0200

    vmware setup

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "Nixos virtual machine";
   
   inputs = {
-    #nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 		nixpkgs.url = "github:nixos/nixpkgs/release-22.05";
 		nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     
@@ -18,7 +17,34 @@
 		overlays = [
 		];		
 	in {
-		nixosConfigurations.parallels-aarch64 = mkVM "parallels-aarch64" {
+    nixosConfigurations.vmware-aarch64 = mkVM "vmware-aarch64" {
+      inherit nixpkgs home-manager;
+      system = "aarch64-linux";
+      user   = "thubie";
+
+      overlays = overlays ++ [(final: prev: {
+        # TODO: drop after release following NixOS 22.05
+        open-vm-tools = inputs.nixpkgs-unstable.legacyPackages.${prev.system}.open-vm-tools;
+
+        # We need Mesa on aarch64 to be built with "svga". The default Mesa
+        # build does not include this: https://github.com/Mesa3D/mesa/blob/49efa73ba11c4cacaed0052b984e1fb884cf7600/meson.build#L192
+        mesa = prev.callPackage "${inputs.nixpkgs-unstable}/pkgs/development/libraries/mesa" {
+          llvmPackages = final.llvmPackages_latest;
+          inherit (final.darwin.apple_sdk.frameworks) OpenGL;
+          inherit (final.darwin.apple_sdk.libs) Xplugin;
+
+          galliumDrivers = [
+            # From meson.build
+            "v3d" "vc4" "freedreno" "etnaviv" "nouveau"
+            "tegra" "virgl" "lima" "panfrost" "swrast"
+
+            # We add this so we get the vmwgfx module
+            "svga"
+          ];
+        };
+      })];
+    };
+		nixosConfigurations.parallels-aarch64 = mkVM "parallels-aarch64" rec {
       inherit overlays nixpkgs home-manager;
       system = "aarch64-linux";
       user   = "thubie";
