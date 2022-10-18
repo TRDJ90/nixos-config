8d0118e593e1b3d88ca12226c70248d15f95f52b
Thubie de Jong <thubie@Thubies-MacBook-Pro.local>
2022-10-18 10:05:11 +0200

    Fixes

--- users/thubie/home-manager.nix
+++ users/thubie/home-manager.nix
@@ -68,6 +68,8 @@
         enable =  true;
         #should use builtins.readdFile to read polybar config        
         script = ''
+            killall polybar
+            polybar -c ~/.config/polybar/config main
         '';
         package = pkgs.polybar.override {
             i3GapsSupport =  true;      
