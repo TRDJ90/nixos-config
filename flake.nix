f8ffbf4f58753ee75dfe933b505b8ae40f377371
Thubie de Jong <thubie@Thubies-MacBook-Pro.local>
2022-10-21 20:01:54 +0200

    Check out overlay and external flake

--- flake.nix
+++ flake.nix
@@ -6,7 +6,10 @@
 		#nixpkgs.url = "github:nixos/nixpkgs/release-22.05";
 		nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     
-    leftwm.url = "github:leftwm/leftwm";
+    leftwm-overlay = {
+      url = "github:leftwm/leftwm";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     home-manager = {
 			url = "github:nix-community/home-manager/release-22.05";
@@ -18,8 +21,8 @@
 		mkVM = import ./lib/mkvm.nix;
 
 		overlays = [
-
-		];		
+      inputs.leftwm-overlay.overlay
+    ];		
 	in {
     nixosConfigurations.parallels-aarch64 = mkVM "parallels-aarch64" {
       inherit overlays nixpkgs home-manager;
